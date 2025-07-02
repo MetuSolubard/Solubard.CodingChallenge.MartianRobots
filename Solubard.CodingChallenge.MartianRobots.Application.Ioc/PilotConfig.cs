bf381972e6f5e6af88719da90bb12084816d212a
MetuSolubard <matthew.lombard@solubard.co.za>
2025-07-02 21:54:42 +0200

    Configured Dependency Injection and separated program logic to make testing easier versus attempting to test static methods.

--- Solubard.CodingChallenge.MartianRobots.Application.Ioc/PilotConfig.cs
+++ Solubard.CodingChallenge.MartianRobots.Application.Ioc/PilotConfig.cs
@@ -1,7 +1,26 @@
-﻿namespace Solubard.CodingChallenges.MartianRobots.Applications.Ioc
-{
-    public class PilotConfig
-    {
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Solubard.CodingChallenge.MartianRobots.Domain.Services;
+using Solubard.CodingChallenge.MartianRobots.Domain.Services.Interfaces;
 
+namespace Solubard.CodingChallenges.MartianRobots.Applications.Ioc
+{
+    public static class PilotConfig
+    {
+        public static IHost ConfigureContainer(string[] args)
+        {
+            var builder = Host.CreateApplicationBuilder(args);
+
+            builder.Services.AddSingleton(new HashSet<(int x, int y)>());
+            builder.Services.AddScoped<ICommandLineService, CommandLineService>();
+            builder.Services.AddScoped<IMovementService, MovementService>();
+
+            builder.Logging.ClearProviders();
+            builder.Logging.AddConsole();
+
+            return builder.Build();
+
+        }
     }
 }
