3bbcf3526b0b1f6d5835613821ce7184d6350748
MetuSolubard <matthew.lombard@solubard.co.za>
2025-07-02 21:52:47 +0200

    Updated Models. Making use of enums.

--- Solubard.CodingChallenge.MartianRobots.Domain.Models/Rover.cs
+++ Solubard.CodingChallenge.MartianRobots.Domain.Models/Rover.cs
@@ -1,11 +1,15 @@
-﻿using Solubard.CodingChallenge.MartianRobots.Domain.Models.Interfaces;
+﻿using Solubard.CodingChallenge.MartianRobots.Domain.Models.Enums;
+using Solubard.CodingChallenge.MartianRobots.Domain.Models.Interfaces;
 
 namespace Solubard.CodingChallenge.MartianRobots.Domain.Model
 {
     public class Rover : IRobot, IRover
     {
-        public char CurrentDirection { get; set; }
+        public Direction CurrentDirection { get; set; }
         public int Odometer { get; set; }
-        public int CurrentPosition { get; set; }
+        public int CurrentXPosition { get; set; }
+        public int CurrentYPosition { get; set; }
+        public bool IsLost { get; set; }
+        public List<Movement> InstructionsProcessed { get; set; }
     }
 }
